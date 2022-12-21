48dfb1e2d5480d1913fd51fa16f4e810d602cf48
Jan Hoffmann <jan@hfmn.pl>
2022-12-21 16:10:06 +0100

    refactor: removed testing wasm from bundled ones

--- client-js/copy-wasms.sh
+++ client-js/copy-wasms.sh
@@ -7,4 +7,3 @@ cp ../client/owner_of_session/target/wasm32-unknown-unknown/release/owner_of_cal
 cp ../client/get_approved_session/target/wasm32-unknown-unknown/release/get_approved_call.wasm ./wasm
 cp ../client/transfer_session/target/wasm32-unknown-unknown/release/transfer_call.wasm ./wasm
 cp ../client/updated_receipts/target/wasm32-unknown-unknown/release/updated_receipts.wasm ./wasm
-cp ../test-contracts/minting_contract/target/wasm32-unknown-unknown/release/minting_contract.wasm ./wasm
