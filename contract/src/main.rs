3a2ee9c4ce1c8799041826ee694b6630ea5091d7
Karan Dhareshwar <karan@casperlabs.io>
2023-02-21 11:18:17 -0600

    Run make lint

--- contract/src/main.rs
+++ contract/src/main.rs
@@ -1456,8 +1456,7 @@ pub extern "C" fn migrate() {
     let migration_flag = utils::has_migration_flag();
     let migration_version = utils::has_migration_version();
 
-
-    if  migration_flag && migration_version{
+    if migration_flag && migration_version {
         runtime::revert(NFTCoreError::ContractAlreadyMigrated)
     }
 
@@ -1501,7 +1500,8 @@ pub extern "C" fn migrate() {
         let new_receipt_string_representation = format!("{CEP78_PREFIX}{collection_name}");
         runtime::put_key(
             &new_receipt_string_representation,
-            storage::new_uref(new_contract_package_hash_representation.to_formatted_string()).into(),
+            storage::new_uref(new_contract_package_hash_representation.to_formatted_string())
+                .into(),
         );
         storage::write(receipt_uref, new_receipt_string_representation);
 
@@ -1510,8 +1510,8 @@ pub extern "C" fn migrate() {
             NFTCoreError::MissingIdentifierMode,
             NFTCoreError::InvalidIdentifierMode,
         )
-            .try_into()
-            .unwrap_or_revert();
+        .try_into()
+        .unwrap_or_revert();
 
         match identifier {
             NFTIdentifierMode::Ordinal => utils::migrate_owned_tokens_in_ordinal_mode(),
