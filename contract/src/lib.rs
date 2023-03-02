a46c42bf299928ffc29495ab39ca962c1dad65e1
gRoussac <gRoussac@users.noreply.github.com>
2023-03-02 20:26:49 +0100

    Merge cargo.tmol changes

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -6,4 +6,8 @@ pub mod constants;
 pub mod error;
 pub mod events;
 pub mod modalities;
+
+// A feature to allow the contract to be used
+// as a library and a binary.
+#[cfg(feature = "contract-support")]
 pub mod utils;
